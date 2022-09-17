e18814d7cf3408875772ab57174ca4dad610e11b
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-09-17 21:53:39 +0900

    [wip] スクレイピングの実装

--- scraping/main.go
+++ scraping/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("Hello")
+	url := "https://tabelog.com/"
+	// url := "https://qiita.com/Yaruki00/items/b50e346551690b158a79"
+	getTabelogScrape(url)
+}
+
+func getTabelogScrape(url string) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	//Bodyを読み込む
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	doc.Find(".list-rst__body").Each(func(_ int, s *goquery.Selection) {
+		fmt.Println(s.Text())
+	})
+	fmt.Println(doc.Text())
 }
